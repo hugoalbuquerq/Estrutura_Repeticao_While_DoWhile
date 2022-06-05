02ff285bc638296aa8e353c8918f362bea286686
Hugo <alheiodef@live.com>
2022-06-05 13:26:17 -0300

    primeiraversao

---
+++ Atv_DoWhile.js
@@ -0,0 +1,29 @@
+// 1)      Faça uma função que exibe todos os números maiores que zero e menores que 10.
+
+function numeros(valor){
+    let num = valor;
+    return num;
+}
+let result = 9
+do {
+    console.log(numeros(result));
+    result--;
+} while (result > 0){
+
+
+}
+
+//2)      Faça uma função que exibe os 10 primeiros números que são menores que 80.
+
+function numeros1(valor){
+    let num = valor;
+    return num;
+}
+let result1 = 79
+do {
+    console.log(numeros1(result1));
+    result1--;
+} while (result1 > 69){
+
+
+}
