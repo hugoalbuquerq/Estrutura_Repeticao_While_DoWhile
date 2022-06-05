3dc96350b7c48f64da570386e9a1d08ee24d7454
Hugo <alheiodef@live.com>
2022-06-05 13:05:17 -0300

    primeiraversao

---
+++ Atv01.js
@@ -0,0 +1,26 @@
+//1)      Faça uma função que exibe todos os números menores que 10.
+function exibirNum (valor){
+    let num = valor;
+    return num;
+}
+
+let cadeiadeNum = 0
+
+while (cadeiadeNum < 10){
+    console.log(exibirNum(cadeiadeNum));
+    cadeiadeNum++;
+}
+
+//2)      Faça uma função que exibe todos os números maiores que 50 e menores que 70.
+
+function exibirNum2 (valor){
+    let num = valor;
+    return num;
+}
+
+let cadeiadeNum2 = 51
+
+while (cadeiadeNum2 > 50 && cadeiadeNum2 < 70){
+    console.log(exibirNum2(cadeiadeNum2));
+    cadeiadeNum++;
+}
